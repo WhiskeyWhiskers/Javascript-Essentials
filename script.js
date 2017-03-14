8711f120144934d7ee86608f5155259711100073
WhiskeyWhiskers <stephen.akerson@gmail.com>
2017-03-14 15:55:44 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function ConstructApple(){
+
+	box = {color: "brown"}
+
+	console.log(box.color);
+
+	this.color = "red";
+	this.width = 200;
+    this.height = 300;
+
+}
+
+ConstructApple.prototype = {
+	eat: function(){ return 'eat that apple'; },
+	throw: function(){ return 'THROW IT!'; },
+	nibble: function(){ return 'nibble like a hamster'; }
+}
+
+var apple = new ConstructApple();
+var appl2 = new ConstructApple();
+var apple3 = new ConstructApple();
+
